7764ded273f015e2fe0ce0c3015207bcb468b79d
KEVIN LI <kevin@KEVINs-MacBook-Pro.local>
2017-02-12 23:36:59 +1100

    Unit 10 completed but some codes do not work.

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,27 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
+
 if (cardTwo === cardFour) {
 	alert('You found a match!')
 } else {
 	alert('Sorry, try again.')
+} 
+
+*/
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function () {
+	for (var i = 0; i =< 4; i++) {
+    var newCards = document.createElement('div');
+	newCards.className = 'card';
+	gameBoard.appendChild(newCards);
+	}
 }
+
+var button = document.querySelector('#button');
+button.addEventListener('click', createCards);
+
+/* doesn't seem to work on the browser... */
